3e72ef7725b7a06f27e7c38ff38bcf5954021ba1
NikitaGoluban <nikitagoluban@proton.me>
2025-10-26 11:43:32 +0100

    all works

--- js/task-2.js
+++ js/task-2.js
@@ -1,12 +1,8 @@
-{
-  "printWidth": 80,
-  "tabWidth": 2,
-  "useTabs": false,
-  "semi": true,
-  "singleQuote": true,
-  "trailingComma": "es5",
-  "bracketSpacing": true,
-  "jsxBracketSameLine": false,
-  "arrowParens": "avoid",
-  "proseWrap": "always"
+function getShippingMessage (country, price, deliveryFee) {
+    const totalPrice = price + deliveryFee;
+    return `Shipping to ${country} will cost ${totalPrice} credits`;
 }
+
+console.log(getShippingMessage("Australia", 120, 50)); 
+console.log(getShippingMessage("Germany", 80, 20)); 
+console.log(getShippingMessage("Sweden", 100, 20)); 
